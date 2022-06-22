014b449b87c9c835a79693b3f4d5892b7e694a79
mishkanchik <csprince666@gmail.com>
2022-06-22 18:11:10 +0300

    commit deploy

---
+++ src/components/FooterComponent/FooterComponent.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Container } from "@mui/system";
+import "components/FooterComponent/FooterComponent.scss";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import { FooterComponentContent } from "components/FooterComponentContent/FooterComponentContent";
+
+export const FooterComponent = () => {
+  return (
+    <>
+      <footer className="FooterComponent-last-content">
+        <Container>
+          <div className="row">
+            <div className="FooterComponent-2021">
+              © 2021 Betheme by <a href="/"> Muffin group</a> | All Rights
+              Reserved | Powered by <a href="/"> WordPress </a>
+            </div>
+            <div className="social">
+              <FacebookOutlinedIcon className="facebook" />
+              <TwitterIcon className="twitter" />
+              <PinterestIcon className="pinterest" />
+              <InstagramIcon className="instagram" />
+            </div>
+          </div>
+        </Container>
+      </footer>
+    </>
+  );
+};
